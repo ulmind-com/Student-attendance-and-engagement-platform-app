60b77ba82d36863287c4e1b4a7e436d94a5f8ac8
Samiran2004 <samiran.samanta.dev@gmail.com>
2026-05-28 19:04:07 +0530

    fix: override tslib resolution in metro config to prevent import errors

--- metro.config.js
+++ metro.config.js
@@ -10,4 +10,14 @@ config.resolver.extraNodeModules = {
   "framer-motion": path.resolve(__dirname, "scripts/framer-motion-stub.js"),
 };
 
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName === "tslib" || moduleName.endsWith("tslib/modules/index.js")) {
+    return {
+      filePath: path.resolve(__dirname, "node_modules/tslib/tslib.es6.js"),
+      type: "sourceFile",
+    };
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
+
 module.exports = withNativeWind(config, { input: "./src/global.css" });
